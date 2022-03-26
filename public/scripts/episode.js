5d2a3cf2c8034b8a2bd0f7d43ede61c0b364ecce
John Pineda <mykie.pineda@gmail.com>
2022-03-26 14:39:55 +1300

    implement custom video player controls

---
+++ public/scripts/episode.js
@@ -0,0 +1,88 @@
+window.addEventListener("load", function () {
+
+    const videoContainer = document.querySelector("#video-container");
+    const videoPlayer = document.querySelector("#video-player");
+    const playPauseButton = document.querySelector("#play-pause-button");
+    const playPauseIcon = playPauseButton.querySelector("i");
+    const volumeButton = document.querySelector("#volume-button");
+    const volumeIcon = volumeButton.querySelector("i");
+    const volumeSlider = document.querySelector("#volume-slider");
+    const current = document.querySelector("#current");
+    const duration = document.querySelector("#duration");
+    const progress = document.querySelector("#video-progress-background");
+    const progressBar = document.querySelector("#video-progress-filled");
+    const fullscreen = document.querySelector("#fullscreen");
+
+    playPauseButton.addEventListener("click", function (event) {
+        if (videoPlayer.paused) {
+            videoPlayer.play();
+            playPauseIcon.classList.remove("fa-play");
+            playPauseIcon.classList.add("fa-pause");
+        } else {
+            videoPlayer.pause();
+            playPauseIcon.classList.add("fa-play");
+            playPauseIcon.classList.remove("fa-pause");
+        }
+    })
+
+    volumeButton.addEventListener("click", function (event) {
+        videoPlayer.muted = !videoPlayer.muted;
+        volumeIcon.classList.toggle("fa-volume-high");
+        volumeIcon.classList.toggle("fa-volume-xmark");
+    });
+
+    volumeSlider.addEventListener("mousemove", function (event) {
+        videoPlayer.volume = event.target.value;
+
+        if (videoPlayer.muted || videoPlayer.volume === 0) {
+            volumeIcon.classList.add("fa-volume-xmark");
+            volumeIcon.classList.remove("fa-volume-high");
+            volumeIcon.classList.remove("fa-volume-low");
+        } else {
+            volumeIcon.classList.remove("fa-volume-xmark");
+            if (videoPlayer.volume <= 0.5) {
+                volumeIcon.classList.remove("fa-volume-high");
+                volumeIcon.classList.add("fa-volume-low");
+            } else if (videoPlayer.volume > 0.5) {
+                volumeIcon.classList.add("fa-volume-high");
+                volumeIcon.classList.remove("fa-volume-low");
+            }
+        }
+    });
+
+    function currentTime() {
+        let currentMinutes = Math.floor(videoPlayer.currentTime / 60);
+        let currentSeconds = Math.floor(videoPlayer.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(videoPlayer.duration / 60);
+        let durationSeconds = Math.floor(videoPlayer.duration - durationMinutes * 60);
+
+        current.innerHTML = `${currentMinutes}:${currentSeconds < 10 ? '0' + currentSeconds : currentSeconds}`;
+        duration.innerHTML = `${durationMinutes}:${durationSeconds < 10 ? '0' + durationSeconds : durationSeconds}`;
+    }
+
+    videoPlayer.addEventListener("timeupdate", currentTime);
+
+    videoPlayer.addEventListener("timeupdate", function (event) {
+        const percentage = (videoPlayer.currentTime / videoPlayer.duration) * 100;
+        progressBar.style.width = `${percentage}%`;
+    });
+
+
+    function progressVideo(event) {
+        const progressTime = (event.offsetX / progress.offsetWidth) * videoPlayer.duration;
+        videoPlayer.currentTime = progressTime;
+    }
+
+    progress.addEventListener("click", function (event) {
+        progressVideo(event);
+    });
+
+    progressBar.addEventListener("click", function (event) {
+        progressVideo(event);
+    });
+
+    fullscreen.addEventListener("click", function () {
+        videoContainer.requestFullscreen();
+    });
+
+});
